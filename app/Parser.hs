419ffe17fc62119a95f373a542373030a233f07b
TomTroppmann <troppmann.tom@gmail.com>
2024-08-07 17:43:48 +0200

    Rewrite Parser to maybe implement Parenthesis

--- app/Parser.hs
+++ app/Parser.hs
@@ -7,22 +7,30 @@ import Representation
 import Text.Read (readMaybe)
 
 parseText :: String -> Expr
-parseText = parse . separate
+parseText = parse Nothing . separate
 
 separate :: String -> [String]
 separate = words
 
 -- TODO Precedence + * or parenthesis
-parse :: [String] -> Expr
-parse [] = error "Error: Expected Expr got ''"
-parse ["True"] = Const (VBool True)
-parse ["False"] = Const (VBool False)
-parse ["Uniform"] = Uniform
-parse ["Normal"] = Normal
-parse (x : "*" : xs) = Multiply (parse [x]) (parse xs)
-parse (x : "+" : xs) = Plus (parse [x]) (parse xs)
-parse (x : "-" : xs) = Subtract (parse [x]) (parse xs)
-parse (x : xs)
-  | xs /= [] = error ("Error: Expected no input got '" <> show xs <> "'")
-  | Just float <- readMaybe x :: Maybe Float = Const (VFloat float)
-  | otherwise = error ("Error: Unknown input '" <> x <> "'")
+parse :: Maybe Expr -> [String] -> Expr
+parse Nothing [] = error "Error: Expected Expr got ''"
+parse (Just expr) [] = expr
+parse Nothing ("True" : xs) = parse (Just $ Const $ VBool True) xs
+parse Nothing ("False" : xs) = parse (Just $ Const $ VBool False) xs
+parse Nothing ("Uniform" : xs) = parse (Just Uniform) xs
+parse Nothing ("Normal" : xs) = parse (Just Normal) xs
+parse (Just _) ("True" : _) = error "Error: Expected Operator got 'True'"
+parse (Just _) ("False" : _) = error "Error: Expected Operator got 'False'"
+parse (Just _) ("Uniform" : _) = error "Error: Expected Operator got 'Uniform'"
+parse (Just _) ("Normal" : _) = error "Error: Expected Operator got 'Normal'"
+parse (Just expr) ("*" : xs) = Multiply expr (parse Nothing xs)
+parse (Just expr) ("+" : xs) = Plus expr (parse Nothing xs)
+parse (Just expr) ("-" : xs) = Subtract expr (parse Nothing xs)
+parse Nothing ("*" : _) = error "Error: Expected Value got Operator '*'"
+parse Nothing ("+" : _) = error "Error: Expected Value got Operator '+'"
+parse Nothing ("-" : _) = error "Error: Expected Value got Operator '-'"
+parse Nothing (unknownString : xs)
+  | Just float <- readMaybe unknownString :: Maybe Float = parse (Just $ Const $ VFloat float) xs
+  | otherwise = error ("Error: Unknown String '" <> unknownString <> "'")
+parse (Just _) (unknownString : _) = error ("Error: Expected Operator got '" <> unknownString <> "'")
