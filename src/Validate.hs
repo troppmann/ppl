e6040a8790d3b07b3860ea151d191fa558ac2c81
TomTroppmann <troppmann.tom@gmail.com>
2024-08-27 18:16:56 +0200

    Add early optimization

--- src/Validate.hs
+++ src/Validate.hs
@@ -1,3 +1,5 @@
+{-# OPTIONS_GHC -Wno-unused-top-binds #-}
+
 module Validate
   ( ValidateInfo (..),
     validateExpr,
@@ -6,7 +8,6 @@ module Validate
 where
 
 import Data.Either
-import Debug.Trace
 import Interpret
 import Representation
 
@@ -22,10 +23,10 @@ validateExpr :: ValidateInfo -> Expr -> Double
 validateExpr info expr = validateFunc info (convertExprToFunction expr)
 
 validateFunc :: ValidateInfo -> (Double -> Double) -> Double
-validateFunc info f = approxIntegral (start info) (end info) (stepWidth info) f
+validateFunc info f = approxIntegral2 (start info) (end info) (stepWidth info) f
 
 convertExprToFunction :: Expr -> (Double -> Double)
-convertExprToFunction expr a = trace ("f with " <> show a) . convertOutput . interpret expr . convertInput $ a
+convertExprToFunction expr = convertOutput . interpret expr . convertInput
   where
     convertInput = VFloat
     convertOutput = fromRight 0.0 . fmap snd
@@ -34,9 +35,25 @@ convertExprToFunction expr a = trace ("f with " <> show a) . convertOutput . int
 approxIntegral :: Double -> Double -> Double -> (Double -> Double) -> Double
 approxIntegral start end stepWidth f
   | start > end = 0.0
-  | otherwise = (trapez f start b) + approxIntegral b end stepWidth f
+  | otherwise = trapez f start b + approxIntegral b end stepWidth f
   where
     b = start + stepWidth
 
 trapez :: (Double -> Double) -> Double -> Double -> Double
 trapez f a b = (b - a) * 0.5 * (f a + f b)
+
+-- TODO: don't execute function twice but utterly unreadable
+-- SpeedUp is measurable
+approxIntegral2 :: Double -> Double -> Double -> (Double -> Double) -> Double
+approxIntegral2 start end stepWidth f = factor * fst (foldr combine (0, 0) values)
+  where
+    values = generateFunctionValues start end stepWidth f
+    factor = 0.5 * stepWidth
+
+combine :: Double -> (Double, Double) -> (Double, Double)
+combine listEntry (acc, previous) = (acc + previous + listEntry, listEntry)
+
+generateFunctionValues :: Double -> Double -> Double -> (Double -> Double) -> [Double]
+generateFunctionValues start end stepWidth f = map f $ take times $ iterate (+ stepWidth) start
+  where
+    times = ceiling $ (end - start) / stepWidth
