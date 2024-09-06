762afb8b1ff58ce6d3d3c059f9e31f1a364a9d3f
TomTroppmann <troppmann.tom@gmail.com>
2024-09-06 13:19:42 +0200

    Add LessEqualThen and GreaterEqualThen

--- src/Interpret.hs
+++ src/Interpret.hs
@@ -9,6 +9,7 @@ import Representation
 import Statistics.Distribution
 import Statistics.Distribution.Normal (normalDistr)
 import Statistics.Distribution.Uniform
+import Prelude hiding (EQ, GT, LT)
 
 interpret :: Expr -> Value -> Either String DimensionalProbability
 interpret Uniform (VFloat f) = Right (1, density distr f)
@@ -82,7 +83,6 @@ interpret (IfElseThen e1 e2 e3) value = do
   (_dim2, p2) <- interpret e2 value
   (_dim3, p3) <- interpret e3 value
   return (1, probTrue * p2 + probFalse * p3)
--- TODO 03.09.2024: Add LessThanEqual and GreaterThanEqual
 interpret (Equal e1 e2) (VBool bool)
   | Right constant <- evalConstExpr e2 = case constant of
       (VFloat c) -> do
@@ -102,6 +102,16 @@ interpret (Equal e1 e2) (VBool bool)
 interpret (Unequal e1 e2) (VBool bool) = do
   (dim, prob) <- interpret (Equal e1 e2) (VBool bool)
   return (dim, 1 - prob)
+interpret (LessEqualThan e1 e2) (VBool bool)
+  | Right constant <- evalConstExpr e2 = do
+      c <- evalAsFloat constant
+      x <- compareFloatExpr e1 (LE, c)
+      return (0, if bool then x else 1 - x)
+  | Right constant <- evalConstExpr e1 = do
+      c <- evalAsFloat constant
+      x <- compareFloatExpr e2 (GE, c)
+      return (0, if bool then x else 1 - x)
+  | otherwise = Left "Can only interpret < with a one side Constant."
 interpret (LessThan e1 e2) (VBool bool)
   | Right constant <- evalConstExpr e2 = do
       c <- evalAsFloat constant
@@ -122,17 +132,31 @@ interpret (GreaterThan e1 e2) (VBool bool)
       x <- compareFloatExpr e2 (LT, c)
       return (0, if bool then x else 1 - x)
   | otherwise = Left "Can only interpret > with a one side Constant."
+interpret (GreaterEqualThan e1 e2) (VBool bool)
+  | Right constant <- evalConstExpr e2 = do
+      c <- evalAsFloat constant
+      x <- compareFloatExpr e1 (GE, c)
+      return (0, if bool then x else 1 - x)
+  | Right constant <- evalConstExpr e1 = do
+      c <- evalAsFloat constant
+      x <- compareFloatExpr e2 (LE, c)
+      return (0, if bool then x else 1 - x)
+  | otherwise = Left "Can only interpret > with a one side Constant."
 interpret (GreaterThan _ _) (VFloat _) = Right (0, 0.0)
 interpret (LessThan _ _) (VFloat _) = Right (0, 0.0)
 interpret (Equal _ _) (VFloat _) = Right (0, 0.0)
 interpret (Unequal _ _) (VFloat _) = Right (0, 0.0)
 interpret e _ = todo ("Missing interpret case: " <> show e)
 
-type CompareQuery = (Ordering, Double)
+data CompareCase = LT | LE | EQ | GE | GT deriving (Ord, Enum, Show, Eq)
 
-swap :: Ordering -> Ordering
+type CompareQuery = (CompareCase, Double)
+
+swap :: CompareCase -> CompareCase
 swap LT = GT
+swap LE = GE
 swap GT = LT
+swap GE = LE
 swap EQ = EQ
 
 -- | Epsilon with IEEE754 doubles \n
@@ -146,20 +170,26 @@ inRange (minA, maxB) value = minA <= value && value <= maxB
 -- TODO 06.09.2024: Should i add a dimension differentiation?
 compareFloatExpr :: Expr -> CompareQuery -> Either String Double
 compareFloatExpr (Const (VBool _)) _ = Left "Expected Float got Bool."
-compareFloatExpr (Const (VFloat constant)) query = case query of
-  (LT, value) -> return $ if constant < value then 1.0 else 0.0
-  (GT, value) -> return $ if constant > value then 1.0 else 0.0
-  (EQ, value) -> return $ if constant == value then 1.0 else 0.0
-compareFloatExpr Uniform query = case query of
-  (LT, value) -> return $ cumulative distr value
-  (GT, value) -> return $ complCumulative distr value
-  (EQ, value) -> return $ if inRange (0.0, 1.0) value then epsilon else 0.0
+compareFloatExpr (Const (VFloat constant)) (ord, value) = return $ case ord of
+  LT -> if constant < value then 1.0 else 0.0
+  LE -> if constant <= value then 1.0 else 0.0
+  GT -> if constant > value then 1.0 else 0.0
+  GE -> if constant >= value then 1.0 else 0.0
+  EQ -> if constant == value then 1.0 else 0.0
+compareFloatExpr Uniform (ord, value) = return $ case ord of
+  LT -> cumulative distr value
+  LE -> cumulative distr value
+  GT -> complCumulative distr value
+  GE -> complCumulative distr value
+  EQ -> if inRange (0.0, 1.0) value then epsilon else 0.0
   where
     distr = uniformDistr 0.0 1.0
-compareFloatExpr Normal query = case query of
-  (LT, value) -> return $ cumulative distr value
-  (GT, value) -> return $ complCumulative distr value
-  (EQ, _value) -> return epsilon
+compareFloatExpr Normal (ord, value) = return $ case ord of
+  LT -> cumulative distr value
+  LE -> cumulative distr value
+  GT -> complCumulative distr value
+  GE -> complCumulative distr value
+  EQ -> epsilon
   where
     distr = normalDistr 0.0 1.0
 compareFloatExpr (Plus e1 e2) (ord, value)
@@ -204,17 +234,17 @@ compareFloatExpr (Divide e1 e2) (ord, value)
           let bound = c / value
           case ord of
             -- Two Ranges either ([-∞, 0] and [bound, +∞]) or ([-∞, bound] and [0, +∞])
-            LT -> do
-              firstRange <- compareFloatExpr e2 (if bound < 0 then LT else GT, bound)
-              secondRange <- compareFloatExpr e2 (if bound < 0 then GT else LT, 0)
+            lt | lt `elem` [LT, LE] -> do
+              firstRange <- compareFloatExpr e2 (if bound < 0 then ord else swap ord, bound)
+              secondRange <- compareFloatExpr e2 (if bound < 0 then swap ord else ord, 0)
               return $ firstRange + secondRange
             -- One Range between Bound and 0 could either been [0, Bound] or [Bound,0]
-            GT -> do
-              lower <- compareFloatExpr e2 (LT, min bound 0)
-              higher <- compareFloatExpr e2 (LT, max bound 0)
+            gt | gt `elem` [GT, GE] -> do
+              lower <- compareFloatExpr e2 (swap ord, min bound 0)
+              higher <- compareFloatExpr e2 (swap ord, max bound 0)
               return $ higher - lower
             -- c / e2 == value => e2 == c / value == bound
-            EQ -> compareFloatExpr e2 (EQ, bound)
+            _eq -> compareFloatExpr e2 (EQ, bound)
   | Right constant <- evalConstExpr e2 = do
       c <- evalAsFloat constant
       compareFloatExpr e1 (if c < 0 then swap ord else ord, value * c)
