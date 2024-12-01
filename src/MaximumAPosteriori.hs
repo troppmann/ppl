418368f69effdbe30291d6234329f5e308c45655
TomTroppmann <troppmann.tom@gmail.com>
2024-12-01 15:04:05 +0100

    Fixes Dimension counting in map

--- src/MaximumAPosteriori.hs
+++ src/MaximumAPosteriori.hs
@@ -1,5 +1,5 @@
 module MaximumAPosteriori
-  ( MaximumAPosteriori.map,
+  ( maxAPost,
   )
 where
 
@@ -8,79 +8,62 @@ import Evaluate (evalArithmetic)
 import Interpret
 import Query
 import Representation
-import Prelude hiding (map)
 
 type ErrorString = String
 
 -- TODO 19.11.24 maybe rename to mle
-map :: Expr -> QueryType -> Either ErrorString (Dimension, Value)
-map (Const c) QAny = return (0, c)
-map Normal QAny = return (1, VFloat 0.0)
+maxAPost :: Expr -> QueryType -> Either ErrorString (Dimension, Value)
+maxAPost (Const c) QAny = return (0, c)
+maxAPost Normal QAny = return (1, VFloat 0.0)
 -- Any value between 0 and 1 is most likely, just picked the middle
-map Uniform QAny = return (1, VFloat 0.5)
-map (Plus e1 e2) QAny = mapArithmetic (+) e1 e2
-map (Subtract e1 e2) QAny = mapArithmetic (-) e1 e2
-map (Multiply e1 e2) QAny = mapArithmetic (*) e1 e2
-map (Divide e1 e2) QAny = mapArithmetic (/) e1 e2
-map (CreateTuple e1 e2) QAny = do
-  (d1, v1) <- map e1 QAny
-  (d2, v2) <- map e2 QAny
+maxAPost Uniform QAny = return (1, VFloat 0.5)
+maxAPost (Plus e1 e2) QAny = mapArithmetic (+) e1 e2
+maxAPost (Subtract e1 e2) QAny = mapArithmetic (-) e1 e2
+maxAPost (Multiply e1 e2) QAny = mapArithmetic (*) e1 e2
+maxAPost (Divide e1 e2) QAny = mapArithmetic (/) e1 e2
+maxAPost (CreateTuple e1 e2) QAny = do
+  (d1, v1) <- maxAPost e1 QAny
+  (d2, v2) <- maxAPost e2 QAny
   return (d1 + d2, VTuple v1 v2)
-map (CreateTuple e1 e2) (QTuple q1 q2) = do
-  (d1, v1) <- map e1 q1
-  (d2, v2) <- map e2 q2
+maxAPost (CreateTuple e1 e2) (QTuple q1 q2) = do
+  (d1, v1) <- maxAPost e1 q1
+  (d2, v2) <- maxAPost e2 q2
   return (d1 + d2, VTuple v1 v2)
-map (IfElseThen e1 e2 e3) query = do
-  dimProbTrue@(dimTrue, probTrue) <- dbg $ interpret e1 (VBool True)
+maxAPost (IfElseThen e1 e2 e3) query = do
+  dimProbTrue@(dimTrue, probTrue) <- interpret e1 (VBool True)
   let dimProbFalse@(dimFalse, probFalse) = (0, 1.0) #-# dimProbTrue
-  (dimThen, probThen) <- dbg $ qInterpret e2 query
-  (dimElse, probElse) <- dbg $ qInterpret e3 query
-  -- let decide if one branch is always picked nonetheless
   if dimTrue < dimFalse && probTrue > 0 || (0, 1.0) == dimProbTrue
     then
-      map e2 query
+      maxAPost e2 query
     else
       if dimFalse < dimTrue && probFalse > 0 || (0, 1.0) == dimProbFalse
         then
-          map e3 query
-        -- if both branches are possible decide on dimensionality of the branch
+          maxAPost e3 query
         else
-          if dbg dimThen < dbg dimElse && dbg probThen > 0
-            then map e2 query
-            else
-              if dimElse < dimThen && probElse > 0
-                then
-                  map e3 query
-                -- if both are the same check if one side is not possible
-                else
-                  if probTrue /= 0 && probThen > 0 && probElse == 0
-                    then
-                      map e2 query
-                    else
-                      if probFalse /= 0 && probElse > 0 && probThen == 0
-                        then
-                          map e3 query
-                        else
-                          Left "IfElseThen is not tractable if the query is not selective."
-map e (QAt v) = do
+          maxAPostIfElseThen (maxAPost e2 query) (maxAPost e3 query)
+maxAPost e (QAt v) = do
   (dim, prob) <- interpret e (VFloat v)
   if prob /= 0.0
     then
       return (dim, VFloat v)
     else
       Left "Value is not possible."
-map e q = todo $ "Missing case" <> show e <> show q
+maxAPost e q = todo $ "Missing case" <> show e <> show q
 
 mapArithmetic :: (Double -> Double -> Double) -> Expr -> Expr -> Either ErrorString (Dimension, Value)
 mapArithmetic op e1 e2 = do
-  (d1, m1) <- map e1 QAny
-  (d2, m2) <- map e2 QAny
+  (d1, m1) <- maxAPost e1 QAny
+  (d2, m2) <- maxAPost e2 QAny
   m3 <- evalArithmetic op m1 m2
   return (d1 + d2, m3)
 
--- could also be defined as an operator #<# in dimProb math
-firstMoreLikelyThenSecond :: DimensionalProbability -> DimensionalProbability -> Bool
-firstMoreLikelyThenSecond (dim1, prob1) (dim2, prob2)
-  | dim1 < dim2 = True
-  | dim1 > dim2 = False
-  | otherwise = prob1 > prob2
+maxAPostIfElseThen :: Either ErrorString (Dimension, Value) -> Either ErrorString (Dimension, Value) -> Either ErrorString (Dimension, Value)
+maxAPostIfElseThen (Left e1) (Left e2) = Left $ show e1 <> " " <> show e2
+maxAPostIfElseThen (Left "Value is not possible.") (Right (dim, value)) = return (dim, value)
+maxAPostIfElseThen (Left e) (Right _) = Left e
+maxAPostIfElseThen (Right (dim, value)) (Left "Value is not possible.") = return (dim, value)
+maxAPostIfElseThen (Right _) (Left e) = Left e
+maxAPostIfElseThen (Right (dim1, value1)) (Right (dim2, value2))
+  | dim1 < dim2 = return (dim1, value1)
+  | dim2 < dim1 = return (dim2, value2)
+  | otherwise = Left "IfElseThen is not tractable if the query is not selective."
--- test/MapTest.hs
+++ test/MapTest.hs
@@ -4,7 +4,7 @@ module MapTest
 where
 
 import Assert
-import MaximumAPosteriori (map)
+import MaximumAPosteriori
 import Parser
 import Representation
 import Shorter
@@ -26,7 +26,7 @@ testMapExprWithName :: ExprString -> TestName -> QueryString -> Value -> TestTre
 testMapExprWithName exprString testName queryString expectedValue = testCase testString $ do
   expr <- assertRight $ parseExpr exprString
   query <- assertRight $ parseQuery queryString
-  (dim, value) <- assertRight $ MaximumAPosteriori.map expr query
+  (dim, value) <- assertRight $ maxAPost expr query
   value @?= expectedValue
   where
     testString = shorter testName <> ":Query " <> shorter queryString
@@ -35,7 +35,7 @@ testMapExprFail :: ExprString -> QueryString -> ErrorString -> TestTree
 testMapExprFail exprString queryString errorString = testCase testString $ do
   expr <- assertRight $ parseExpr exprString
   query <- assertRight $ parseQuery queryString
-  MaximumAPosteriori.map expr query @?= Left errorString
+  maxAPost expr query @?= Left errorString
   where
     testString = shorter exprString <> ":Query " <> shorter queryString <> ":Expected Fail"
 
@@ -50,18 +50,19 @@ tests =
           testMapExpr "Normal * 3 + 7" "_" (VFloat 7.0),
           testMapExpr "(Normal + 7) * 3" "_" (VFloat 21.0),
           testMapExpr "((Normal + 7) * 3 - 1) / 2" "_" (VFloat 10.0),
-          testMapExpr "if Uniform < 0.2 then 3 * 9 else 7" "_" (VFloat 7.0),
-          testMapExpr "if Uniform < 0.3 then 5 else (if Uniform < 0.49 then 5 else 6)" "_" (VFloat 5),
+          testMapExprFail "if Uniform < 0.2 then 3 * 9 else 7" "_" "IfElseThen is not tractable if the query is not selective.",
+          testMapExprFail "if Uniform < 0.3 then 5 else (if Uniform < 0.49 then 2 else 6)" "_" "IfElseThen is not tractable if the query is not selective.",
           testMapExpr "if Uniform == 0.5 then 10 else 0" "_" (VFloat 0.0),
           testMapExpr "if Uniform != 3 then 10 else 0" "_" (VFloat 10.0),
           testMapExpr "if Uniform < 0.5 then 3 else Normal" "_" (VFloat 3.0),
-          testMapExpr "if Uniform < 0.5 then Normal + 0.01 else Normal - 0.01" "_" (VFloat 0.0),
+          testMapExprFail "if Uniform < 0.5 then Normal + 0.01 else Normal - 0.01" "_" "IfElseThen is not tractable if the query is not selective.",
           testMapExpr "(Normal * 10 + 5, Normal * 5)" "_" (VTuple (VFloat 5) (VFloat 0)),
-          testMapExpr "(Normal * 10 + 5, if Uniform > 0.7 then 8 else 0)" "_" (VTuple (VFloat 5) (VFloat 0)),
-          testMapExpr "(if Uniform > 0.7 then 8 else 0, if Uniform > 0.7 then 8 else 0)" "_" (VTuple (VFloat 0) (VFloat 0)),
-          testMapExpr "(Normal, if Uniform > 0.7 then 8 else 0, 8)" "_" (VTuple (VFloat 0) (VTuple (VFloat 0) (VFloat 8))),
+          testMapExpr "(Normal * 10 + 5, if Uniform > 0.7 then Normal + 8 else 0)" "_" (VTuple (VFloat 5) (VFloat 0)),
+          testMapExpr "(if Uniform > 0.7 then 0 else Normal, if Uniform == 0.7 then 8 else 0)" "_" (VTuple (VFloat 0) (VFloat 0)),
+          testMapExpr "(Normal, if Uniform > 0.7 then Normal else 0, 8)" "_" (VTuple (VFloat 0) (VTuple (VFloat 0) (VFloat 8))),
           testMapExpr "if (Uniform < 0.4) || (Uniform < 0.4) then (5,5) else (Normal + 10, 10)" "_" (VTuple (VFloat 5) (VFloat 5)),
-          testMapExpr "if Normal < 0.1 then (if Uniform == 0.4 then 5 else 8,5) else (10, 10)" "_" (VTuple (VFloat 8) (VFloat 5))
+          testMapExpr "if Uniform < 0.6 then Normal * 10 else 1 + (Normal * 0.0) " "_" (VFloat 1.0),
+          testMapExpr "if Normal != 0.0 then (if Uniform == 0.4 then 5 else 8, 5) else (10, 10)" "_" (VTuple (VFloat 8) (VFloat 5))
         ],
       testGroup
         "MAP with evidence p(q,e)"
@@ -73,9 +74,7 @@ tests =
           testMapExpr "(Normal, Normal)" "(3, _)" (VTuple (VFloat 3) (VFloat 0)),
           testMapExpr "(Normal, Normal + 7)" "(3, _)" (VTuple (VFloat 3) (VFloat 7)),
           testMapExpr "if Uniform < 0.5 then (Normal + 3, 10) else (Normal + 7, 0)" "(_, 10)" (VTuple (VFloat 3) (VFloat 10)),
-          testMapExpr "if Uniform < 0.5 then (Normal + 3, 10) else (Normal + 7, 0)" "(3, _)" (VTuple (VFloat 3) (VFloat 10)),
-          testMapExpr "if Uniform < 0.6 then Normal * 10 else 0.1 * Normal + 1 " "_" (VFloat 1.0),
-          testMapExpr "if Uniform < 0.4 then (Normal + 3, 10) else (Normal + 7, 0)" "(3, _)" (VTuple (VFloat 3) (VFloat 0)),
+          testMapExprFail "if Uniform < 0.4 then (Normal + 3, 10) else (Normal + 7, 0)" "(3, _)" "IfElseThen is not tractable if the query is not selective.",
           testMapExpr "if Uniform < 0.5 then (Normal + 3, 10) else (3, 0)" "(3, _)" (VTuple (VFloat 3) (VFloat 0))
         ]
     ]
